1a1676dde53e347b315331f36b7bd5c4b6b56e86
Atharva Bhutani <atharvaxcode@proton.me>
2025-08-03 17:19:56 +0530

    Wrote the binary cross-entropy loss

---
+++ src/losses.c
@@ -0,0 +1,15 @@
+#include "../include/engine.h"
+
+uint8_t cross_entropy(bool** inputs, bool** targets, uint8_t n_neurons, uint8_t n_layers)
+{
+	uint8_t xnor_sum = 0;
+	for (int i = 0; i < n_layers; ++i)
+	{
+		for (int j = 0; j < n_neurons; ++j)
+		{
+			xnor_sum += ~(inputs[i][j] ^ targets[i][j]) & 0x1;
+		}
+	}
+
+	return xnor_sum / (n_neurons * n_layers);
+}
