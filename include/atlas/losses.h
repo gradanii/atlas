5cc25e343ac80433e9c35be0366d7f40857acce8
Atharva Bhutani <atharvaxcode@proton.me>
2025-08-11 20:53:37 +0530

    Purely functional

---
+++ include/atlas/losses.h
@@ -0,0 +1,3 @@
+#include "atlas/engine.h"
+
+uint8_t cross_entropy(bool** inputs, bool** targets, uint8_t n_neurons, uint8_t n_layers);
--- src/losses.c
+++ src/losses.c
@@ -1,4 +1,4 @@
-#include "atlas/engine.h"
+#include "atlas/losses.h"
 
 uint8_t cross_entropy(bool** inputs, bool** targets, uint8_t n_neurons, uint8_t n_layers)
 {
